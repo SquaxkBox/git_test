5879eacf2003cc4c61f59bda21b125f2b76b775a
earl <45638229+SquaxkBox@users.noreply.github.com>
2024-12-20 17:17:51 -0500

    Commented out Outline of back_up.sh

--- back_up.sh
+++ back_up.sh
@@ -1,7 +1,8 @@
-look at files in directory: Get file last change time
-    get file last change time on git hub
-        if file is not on git hub get name
-            git add file_name
-            git commit -m " File_name added on DATE TIME"
-        else commit -m "File_name updated DATE TIME"
-        
+#look at files in directory: Get file last change time
+#    get file last change time on git hub
+#        if file is not on git hub get name
+#            git add file_name
+#            git commit -m " File_name added on DATE TIME"
+#       else commit -m "File_name updated DATE TIME"
+#       ls -s
+
