ca5d21279de05a5688f3d7daf7a09f1e1d6cd059
earl <45638229+SquaxkBox@users.noreply.github.com>
2024-12-20 16:48:25 -0500

    back_up.sh structure outlined

---
+++ back_up.sh
@@ -0,0 +1,7 @@
+look at files in directory: Get file last change time
+    get file last change time on git hub
+        if file is not on git hub get name
+            git add file_name
+            git commit -m " File_name added on DATE TIME"
+        else commit -m "File_name updated DATE TIME"
+        
